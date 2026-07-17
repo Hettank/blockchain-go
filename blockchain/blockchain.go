45b85f2e2a9fdc1bab52a123b9e05b39cf8380d8
Hettank <hettank34@gmail.com>
2026-07-17 14:54:15 +0530

    Implement blockchain structure and main functionality

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,1 +1,42 @@
 package blockchain
+
+import "fmt"
+
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	genesisBlock := NewBlock("Genesis Block", []byte{})
+
+	return &Blockchain{
+		Blocks: []*Block{genesisBlock},
+	}
+}
+
+func (bc *Blockchain) GetLatestBlock() *Block {
+	return bc.Blocks[len(bc.Blocks)-1]
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	latestBlock := bc.GetLatestBlock()
+
+	newBlock := NewBlock(data, latestBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func (bc *Blockchain) PrintBlockchain() {
+	fmt.Println("=========================")
+
+	for i, block := range bc.Blocks {
+		fmt.Printf("\n Block %d:\n", i)
+		fmt.Printf("   Address: %p\n", block)
+		fmt.Printf("   Data: %s\n", block.Data)
+		fmt.Printf("   Hash: %x\n", block.Hash)
+
+		if i > 0 {
+			fmt.Printf("   Previous Hash: %x\n", block.PrevHash)
+		}
+	}
+	fmt.Println("=========================")
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package blockchain
+package main
+
+import (
+	"fmt"
+
+	"github.com/Hettank/blockchain-go/blockchain"
+)
+
+func main() {
+	bc := blockchain.NewBlockchain()
+
+	for i := range 10 {
+		bc.AddBlock(fmt.Sprintf("This is block %d", i+1))
+	}
+
+	bc.PrintBlockchain()
+}
